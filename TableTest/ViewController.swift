7287b80d8dc4df25e0919acc493d6f6e597f7cae
Matt <matthew.salt@gmail.com>
2017-03-02 13:58:03 +0000

    Initial Commit

---
+++ TableTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TableTest
+//
+//  Created by Matthew on 02/03/2017.
+//  Copyright © 2017 Matthew. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
